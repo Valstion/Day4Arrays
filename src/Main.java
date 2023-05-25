f11c24847dca6f74de7af5a93fca413712ccd5a2
Valstion <lordvalstion@gmail.com>
2023-05-25 14:30:30 +0300

    14:30

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
         System.out.println(numbers2[3]);
         System.out.println(numbers2[4]);
         System.out.println(numbers2[5]);
-        String[] names = {"Jonas","Petras","Adele","A","K"};
+        String[] names = {"Jonas", "Petras", "Adele", "A", "K"};
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
@@ -29,14 +29,38 @@ public class Main {
         System.out.println(names[4]);
         String[] surnames = new String[40];
         System.out.println(surnames[0]);
-        int [] [] arr2d = {{1,2,3},{4,3,5},{4,3,5},{4,3,5},{4,3,5},{4,3,5},{4,3,5}};
+        int[][] arr2d = {{1, 2, 3}, {4, 3, 5}, {4, 3, 5}, {4, 3, 5}, {4, 3, 5}, {4, 3, 5}, {4, 3, 5}};
         System.out.println(arr2d[1][2]);
         System.out.println("___________________________________________________________");
-        String[] names1 = {"Jonas","Petras","trys","One","two"};
+        String[] names1 = {"Jonas", "Petras", "trys", "One", "two"};
         System.out.println(names1[0]);
         System.out.println(names1[1]);
         System.out.println(names1[2]);
         System.out.println(names1[3]);
         System.out.println(names1[4]);
+        System.out.println("___________________________________________________________");
+        for (int i = 0; i < names1.length; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println("Emaxer = " + i);
+            System.out.println(names1[i]);
+        }
+        int count = 0;
+        double sum = 0;
+        while (true) {
+
+            double rndNum = Math.random();
+            count++;
+            sum += rndNum;
+            System.out.println(rndNum);
+            if (rndNum >= 0.9) {
+                break;
+            }
+
+        }
+        System.out.println(count);
+        System.out.println(sum);
+        System.out.println(sum / count);
     }
 }
